ca92db2f9b885e9935a300d28a6e3742731b55af
Jarrett Dowling <104020555+jarrettdowling@users.noreply.github.com>
2024-07-30 10:27:22 -0400

    update common tests
    updates the common tests file to work with the updated task struct.

--- rustic_api/tests/common/mod.rs
+++ rustic_api/tests/common/mod.rs
@@ -16,24 +16,31 @@ pub fn setup_test_db(task_list: &mut Vec<task::Task>) -> Result<Connection> {
             id integer primary key,
             title text not null,
             priority integer not null,
-            duedate text not null)"
+            parts integer not null,
+            duedate text not null,
+            description text not null)"
         , (),)?;
 
     // create test tasks
-    let test_task_1 = task::Task::new("Test Task 1".to_string(), 0,
-                                        "Fri, 14, Jul 2017 02:40:00 +0000".to_string());
+    let test_task_1 = task::Task::new("Test Task 1".to_string(), 0, 1,
+                                        "Fri, 14, Jul 2017 02:40:00 +0000".to_string(),
+                                        "Test Task 1 Description".to_string());
     thread::sleep(ten_millis);
-    let test_task_2 = task::Task::new("Test Task 2".to_string(), 1,
-                                        "Fri, 14, Jul 2017 02:50:00 +0000".to_string());
+    let test_task_2 = task::Task::new("Test Task 2".to_string(), 1, 2,
+                                        "Fri, 14, Jul 2017 02:50:00 +0000".to_string(),
+                                        "Test Task 2 Description".to_string());
     thread::sleep(ten_millis);
-    let test_task_3 = task::Task::new("Test Task 3".to_string(), 2,
-                                        "Fri, 14, Jul 2017 03:00:00 +0000".to_string());
+    let test_task_3 = task::Task::new("Test Task 3".to_string(), 2, 3,
+                                        "Fri, 14, Jul 2017 03:00:00 +0000".to_string(),
+                                        "Test Task 3 Description".to_string());
     thread::sleep(ten_millis);
-    let test_task_4 = task::Task::new("Test Task 4".to_string(), 1,
-                                        "Fri, 14, Jul 2017 03:10:00 +0000".to_string());
+    let test_task_4 = task::Task::new("Test Task 4".to_string(), 1, 4,
+                                        "Fri, 14, Jul 2017 03:10:00 +0000".to_string(),
+                                        "Test Task 4 Description".to_string());
     thread::sleep(ten_millis);
-    let test_task_5 = task::Task::new("Test Task 5".to_string(), 0,
-                                        "Fri, 14, Jul 2017 03:20:00 +0000".to_string());
+    let test_task_5 = task::Task::new("Test Task 5".to_string(), 0, 5,
+                                        "Fri, 14, Jul 2017 03:20:00 +0000".to_string(),
+                                        "Test Task 5 Description".to_string());
 
     // input into db
     db::create_db_record(&mut conn, &test_task_1)?;
